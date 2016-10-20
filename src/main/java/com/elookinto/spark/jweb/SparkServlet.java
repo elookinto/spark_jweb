45da0436762b4913876fbc27b74b9a0a79ef8846
Paul Wu <zhou.wu@northseattlecolleges.edu>
2016-10-19 22:21:46 -0700

    Initialize.

--- src/main/java/com/elookinto/spark/jweb/SparkServlet.java
+++ src/main/java/com/elookinto/spark/jweb/SparkServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.elookinto.spark.web;
+package com.elookinto.spark.jweb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
